f173917f4bccf418348a1f6f9609cd79c859e7ef
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2023-11-07 00:04:36 +0800

    Added Passpot JWT

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,9 +3,17 @@ import { AuthService } from './auth.service';
 import { UserModule } from 'src/user/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [UserModule, PassportModule],
-  providers: [AuthService, LocalStrategy]
+  imports: [UserModule, PassportModule,JwtModule.register({
+    secret: process.env.JWT_SECRET,
+    signOptions: {expiresIn: "7d"}
+
+  })],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  controllers: [AuthController]
 })
 export class AuthModule {}
